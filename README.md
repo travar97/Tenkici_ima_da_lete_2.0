6f6771a349921fb415bc31ca5f6c2855db28b943
Andrej <andrej.lojdl@gmail.com>
2015-05-28 15:35:59 +0200

    Added parallel comparator to main file.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-*Register (representing sprite object):
+#Notes
+* Register (representing sprite object)
 	- x_coordinate [2B]
 	- Y_coordinate [2B]
 	- Rotation     [1B]
 	- Size&En	   [1B]
 	- Pointer      [2B]
- NOTE: MSB - En; LSB - Size;
+ **NOTE:** MSB - En; LSB - Size;
 
--- battle_city_vhdl/battle_city.vhd
+++ battle_city_vhdl/battle_city.vhd
@@ -8,9 +8,10 @@ entity battle_city is
 		DATA_WIDTH				: natural := 32;
 		COLOR_WIDTH				: natural := 24;
 		ADDR_WIDTH				: natural := 13;
-		REGISTER_NUMBER		: natural := 10;
 		REGISTER_OFFSET      : natural := 0;
-		C_BASEADDR				: natural := 0
+		C_BASEADDR				: natural := 0;
+		REGISTER_NUMBER		: natural := 10;
+		NUM_BITS_FOR_REG_NUM	: natural := 4
 	);
    Port (
       clk_i    		: in  std_logic;
@@ -27,14 +28,27 @@ end battle_city;
 
 architecture Behavioral of battle_city is
 
-   type registers_t is array (0 to REGISTER_NUMBER-1) of unsigned (31 downto  0);
-
+   type registers_t is array (0 to REGISTER_NUMBER-1) of unsigned (63 downto  0);
+	type coordinate_t is array (0 to REGISTER_NUMBER-1) of unsigned (15 downto 0);
+	type pointer_t is array (0 to REGISTER_NUMBER-1) of unsigned (15 downto 0);
+	type rotation_t is array (0 to REGISTER_NUMBER-1) of unsigned (7 downto 0);
+	type size_t is array (0 to REGISTER_NUMBER-1) of unsigned (7 downto 0);
 
 	signal registers_s		: registers_t;
 	signal local_addr_s		: signed(31 to 0);	
-   signal regs_gt_0			: std_logic_vector(0 to REGISTER_NUMBER-1) ;	
 	signal counter_s 			: unsigned(1 downto 0);
 	signal next_counter_s   : unsigned(1 downto 0);
+	signal reg_row_s        : coordinate_t;
+	signal reg_col_s			: coordinate_t;
+	signal reg_size_s			: size_t;
+	signal reg_en_s			: std_logic_vector(REGISTER_NUMBER-1 downto 0);
+	signal reg_rot_s			: rotation_t;
+	signal reg_pointer_s    : pointer_t;
+	signal reg_end_row_s		: coordinate_t;
+	signal reg_end_col_s		: coordinate_t;
+	signal reg_intsect_s		: std_logic_vector(REGISTER_NUMBER-1 downto 0);
+	signal reg_intersected_s: unsigned(NUM_BITS_FOR_REG_NUM-1 downto 0);
+	signal thrd_stg_addr_s  : unsigned(12 downto 0);
 
 begin
 
@@ -43,9 +57,23 @@ begin
 ----------------------------------------
 
 comp_gen: for i in 0 to REGISTER_NUMBER-1 generate
-	  regs_gt_0(i) <= '1' when registers_s(i) > to_unsigned(0, 32) else 
-							'0';
- end generate comp_gen;
+	-- Slice out data from registers --
+	reg_row_s(i)		<= registers_s(i)(63 downto 48);
+	reg_col_s(i)		<= registers_s(i)(47 downto 32);
+	reg_size_s(i)		<= '0' & registers_s(i)(30 downto 24);
+	reg_en_s(i)			<= registers_s(i)(31);
+	reg_rot_s(i)		<= registers_s(i)(23 downto 16);
+	reg_pointer_s(i)	<= registers_s(i)(15 downto 0);
+	
+	-- Prepare some additional data, based on kown values --
+	reg_end_row_s(i) <= reg_row_s(i) + reg_size_s(i);
+	reg_end_col_s(i) <= reg_col_s(i) + reg_size_s(i);
+	reg_intsect_s(i) <= '1' when (unsigned(pixel_row_i) >= reg_row_s(i) and 
+											unsigned(pixel_row_i) < reg_end_row_s(i) and
+											unsigned(pixel_col_i) >= reg_col_s(i) and 
+											unsigned(pixel_col_i) < reg_end_col_s(i)) else
+								'0';	
+end generate comp_gen;
 
 -- Write data from C --
 process(clk_i) begin
@@ -68,11 +96,22 @@ process(clk_i) begin
 end process;
 
 -- Combinational --
-
 next_counter_s <= "00" when counter_s = "11" else
-                   counter_s + 1;
-						 
-local_addr_s <= signed(bus_addr_i) - C_BASEADDR;    
+                   counter_s + 1;						 
+local_addr_s <= signed(bus_addr_i) - C_BASEADDR;  
+-- TO DO: Find a better way to write this statement. --  
+reg_intersected_s <= "1001" when reg_intsect_s(9) = '1' else
+							"1000" when reg_intsect_s(8) = '1' else
+							"0111" when reg_intsect_s(7) = '1' else
+							"0110" when reg_intsect_s(6) = '1' else
+							"0101" when reg_intsect_s(5) = '1' else
+							"0100" when reg_intsect_s(4) = '1' else
+							"0011" when reg_intsect_s(3) = '1' else
+							"0010" when reg_intsect_s(2) = '1' else
+							"0001" when reg_intsect_s(1) = '1' else
+							"0000";
+							
+thrd_stg_addr_s <= reg_pointer_s(to_integer(reg_intersected_s));
 
 ----------------------------------------
 --            SECOND STAGE            --
