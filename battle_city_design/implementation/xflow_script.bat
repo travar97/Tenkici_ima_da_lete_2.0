1814a3082590f15247ccb176c82ef835a11c7be0
Sandra995 <tripkovic.s95@gmail.com>
2017-05-10 10:52:22 +0200

    Podesen workspace

---
+++ battle_city_design/implementation/xflow_script.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+@REM ###########################################
+@REM # Script file to run the flow 
+@REM # 
+@REM ###########################################
+@REM #
+@REM # Command line for ngdbuild
+@REM #
+ngdbuild -p xc6slx45fgg676-2 -nt timestamp -bm battle_city.bmm "D:/RA512014/LPRS2/battle_city_fpga/battle_city_design/implementation/battle_city.ngc" -uc battle_city.ucf battle_city.ngd 
+
+@REM #
+@REM # Command line for map
+@REM #
+map -o battle_city_map.ncd -w -pr b -ol high -timing -detail battle_city.ngd battle_city.pcf 
+
+@REM #
+@REM # Command line for par
+@REM #
+par -w -ol high battle_city_map.ncd battle_city.ncd battle_city.pcf 
+
+@REM #
+@REM # Command line for post_par_trce
+@REM #
+trce -e 3 -xml battle_city.twx battle_city.ncd battle_city.pcf 
+
